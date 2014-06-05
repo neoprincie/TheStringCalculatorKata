eb903f3b24d14d3aa6ef95e9a9330fb4bf7341b7
bprince <bprince@foxguardsolutions.com>
2014-06-05 13:48:48 -0400

    Added first test

---
+++ TheStringCalculatorKata.Tests/StringCalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheStringCalculatorKata.Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [Test]
+        public void AddShouldBeTakeStringOfNumbersAndReturnInt()
+        {
+            var calculator = new StringCalculator();
+            Assert.That(calculator.Add(String.Empty), Is.InstanceOf<Int32>());
+        }
+    }
+}
